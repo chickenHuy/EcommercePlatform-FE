c87f2235aeb8677c45ab7fb8407f1e8074475baf
chickenHuy <nguyenhuythanh256@gmail.com>
2024-09-14 20:58:47 +0700

    Create and design UI for admin, vendor, customer.

---
+++ src/app/[locale]/auth/page.jsx
@@ -0,0 +1,10 @@
+import AuthPage from "@/views/authPage";
+
+export default function Auth() {
+
+  return (
+    <div className="bg-white-secondary">
+      <AuthPage />
+    </div>
+  );
+}
---
+++ src/app/[locale]/vendor/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import VendorNavigate from '@/components/navigates/vendorNavigate';
+
+export default function VendorDashboard() {
+    return (
+        <div className='w-ful h-fullflex flex-row justify-center items-center'>
+            <div className='w-fit h-fit'>
+            <VendorNavigate />
+            </div>
+            <div className='flex-grow'>
+
+            </div>
+        </div>
+    )
+}
