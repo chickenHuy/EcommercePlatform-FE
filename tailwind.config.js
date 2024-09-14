e60a3d7192d7159dbc9c37e49aebeed2d3bafd59
chickenHuy <nguyenhuythanh256@gmail.com>
2024-09-14 20:54:15 +0700

    Install and setup Tailwind CSS for website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
+    colors: {
+      "black-primary": "#1f1f1f",
+      "black-secondary": "#292929",
+      "black-tertiary": "#3d3d3d",
+
+      "white-primary": "#ffffff",
+      "white-secondary": "#e6e6e6",
+      "white-tertiary": "#828282",
+
+      "gray-primary": "#c0c0c0",
+      "gray-secondary": "#adadad",
+      "gray-tertiary": "#959595",
+
+      "transparent-primary": "#00000000",
+    }
   },
   plugins: [],
 };
