81d635c3198c1b440051e6eaa685c7932cb9db4f
Khan9Tran <trankhangtctv@gmail.com>
2024-11-05 16:15:13 +0700

    add fb login

---
+++ src/app/[locale]/facebook-authenticate/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { authenticateWithFacebook } from "@/api/auth/oauthRequest";
+import { handleLoginNavigation } from "@/utils/auth";
+import { useRouter } from "next/navigation";
+
+export default function Authenticate() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const authCodeRegex = /code=([^&]+)/;
+    const isMatch = window.location.href.match(authCodeRegex);
+
+    if (isMatch) {
+      const authCode = isMatch[1];
+
+      authenticateWithFacebook(authCode)
+        .then((response) => {
+          handleLoginNavigation(response.result.token, router);
+        })
+        .catch((error) => {
+          console.error("Authentication failed:", error);
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress></CircularProgress>
+        <Typography>Authenticating...</Typography>
+      </Box>
+    </>
+  );
+}
