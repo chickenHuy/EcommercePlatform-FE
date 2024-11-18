423c70731c370e328ecc276980f80f14f7b5577f
chickenHuy <nguyenhuythanh256@gmail.com>
2024-11-18 20:06:10 +0700

    Tạo API createProduct và các API upload hình ảnh, video của sản phẩm.

--- src/api/vendor/productRequest.js
+++ src/api/vendor/productRequest.js
@@ -1,27 +1,36 @@
-import { del, patch, get } from "@/lib/httpClient";
+import { del, patch, get, post } from "@/lib/httpClient";
 
 export const getProducts = async (page, sortBy, order, tab, search) => {
-    try {
-        const response = await get(
-          `/api/v1/products/?tab=${tab}&page=${page}&size=10&sortBy=${sortBy}&order=${order}&search=${search}`
-        );
-        return response;
-    } catch (error) {
-        console.error("Error during get products:", error);
-        throw error;
-    }
+  try {
+    const response = await get(
+      `/api/v1/products/?tab=${tab}&page=${page}&size=10&sortBy=${sortBy}&order=${order}&search=${search}`
+    );
+    return response;
+  } catch (error) {
+    console.error("Error during get products:", error);
+    throw error;
+  }
 }
+export const createProduct = (data) => {
+  try {
+    const response = post(`/api/v1/products`, data);
+    return response;
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    throw error;
+  }
+};
 
 export const updateProductStatus = async (id) => {
-    try {
-      const response = await patch(
-        `/api/v1/products/${id}/status`
-      );
-      return response;
-    } catch (error) {
-      console.error("Error during get products:", error);
-      throw error;
-    }
+  try {
+    const response = await patch(
+      `/api/v1/products/${id}/status`
+    );
+    return response;
+  } catch (error) {
+    console.error("Error during get products:", error);
+    throw error;
+  }
 }
 
 export const deleteProduct = async (id) => {
@@ -30,6 +39,47 @@ export const deleteProduct = async (id) => {
     return response;
   } catch (error) {
     console.error("Error during get products:", error);
+    throw error;
+  }
+};
+
+export const uploadMainProductImage = async (file, productId) => {
+  try {
+    const formData = new FormData();
+    formData.append("mainImage", file);
+
+    const response = await post(`/api/v1/images/products/${productId}`, formData);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const uploadListProductImage = async (listFile, productId) => {
+  try {
+    console.log(listFile);
+    const formData = new FormData();
+
+    listFile.forEach(file => {
+      formData.append("images", file); 
+    });
+
+    const response = await post(`/api/v1/images/products/${productId}/list`, formData);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const uploadMainProductVideo = async (file, productId) => {
+  try {
+    const formData = new FormData();
+    formData.append("videoFile", file);
+
+    const response = await post(`/api/v1/videos/products/${productId}`, formData);
+    return response;
+  } catch (error) {
     throw error;
   }
 };
