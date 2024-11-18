4604e6076ef970321fe5b3d2a0a5636366653486
Khan9Tran <trankhangtctv@gmail.com>
2024-11-18 21:42:12 +0700

    fix logo

--- src/components/headers/userHeader.jsx
+++ src/components/headers/userHeader.jsx
@@ -11,19 +11,16 @@ const UserHeader = () => {
         <header className="w-full bg-black-primary text-white rounded-lg">
           <div className="flex h-16 items-center justify-between px-8">
             <div className="hidden md:flex items-center font-bold">
-              {[{ name: "HKUptech", path: "/" }].map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.path}
-                  className="px-5 text-sm text-white-primary hover:text-white-tertiary transition-colors"
-                >
-                  {item.name}
-                </Link>
-              ))}
+              <Link
+                href="/"
+                className="px-5 text-sm text-white-primary hover:text-white-tertiary transition-colors"
+              >
+                HK-Uptech
+              </Link>
             </div>
             <div className="w-full max-w-[480px] m-10 h-8 bg-black-secondary outline-2 outline-black-secondary  relative rounded-lg">
               <Input className="bg-black-secondary outline-1 outline-black-secondary text-white-primary w-full h-full pr-2 pl-2 rounded-lg font-light" />
-              <button className="absolute top-0 right-0 h-full pd-2 text-white-primary rounded-r-md hover:text-gray-tertiary">
+              <button className="absolute top-0 right-0 h-full p-2 text-white-primary rounded-r-md hover:text-gray-tertiary">
                 <Search className="h-full text-white-primary" />
                 <span className="sr-only">Search</span>
               </button>
