108fd0566b07af8991577c0aeaf568659f41704d
chickenHuy <nguyenhuythanh256@gmail.com>
2025-03-07 15:03:12 +0700

    Update UI of the form register store.

---
+++ src/app/[locale]/register-store/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Register Store",
+    description: "The page where users can register their store."
+};
+
+export default function RegisterStoreLayout({ children }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
--- src/app/[locale]/register-store/page.jsx
+++ src/app/[locale]/register-store/page.jsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea"
 import {
   getAccountStore,
   registerStore,
@@ -16,29 +17,32 @@ import { useToast } from "@/hooks/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 import { useRouter } from "next/navigation";
 import { refreshToken } from "@/lib/httpClient";
-import { CircularProgress } from "@mui/material";
-import { Textarea } from "@/components/ui/textarea";
+import { useTranslations } from "next-intl";
+import Loading from "@/components/loading";
 
-const registerStoreSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(3, {
-      message: "Tên cửa hàng phải từ 3 đến 30 ký tự",
-    })
-    .max(30, {
-      message: "Tên cửa hàng phải từ 3 đến 30 ký tự",
-    }),
-  bio: z.string().trim().max(255, {
-    message: "Bio không được vượt quá 255 ký tự",
-  }),
-});
 
 export default function RegisterStore() {
   const [username, setUsername] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { toast } = useToast();
   const router = useRouter();
+  const t = useTranslations("VendorRegister");
+
+  const registerStoreSchema = z.object({
+    name: z
+      .string()
+      .trim()
+      .min(3, {
+        message: t("store_name_validation"),
+      })
+      .max(30, {
+        message: t("store_name_validation"),
+      }),
+    bio: z.string().trim().max(255, {
+      message: t("bio_validation"),
+    }),
+  });
 
   const registerStoreForm = useForm({
     resolver: zodResolver(registerStoreSchema),
@@ -69,6 +73,7 @@ export default function RegisterStore() {
   }, [toast]);
 
   const onSubmit = async (data) => {
+    setIsSubmitting(true);
     try {
       await registerStore(data);
       toast({
@@ -84,78 +89,79 @@ export default function RegisterStore() {
         variant: "destructive",
       });
     }
+    finally {
+      setIsSubmitting(false);
+    }
   };
 
   const handleCancel = () => {
-    router.push("/auth");
+    if (isSubmitting) return;
+    router.push("/");
   };
 
   return (
-    <main className="flex items-center justify-center min-h-screen p-4">
+    <main className="flex items-start justify-center min-h-screen p-5 pt-36">
       <Toaster />
       {isLoading ? (
-        <div className="fixed inset-0 flex flex-col justify-center items-center z-[100] space-y-4 bg-black-secondary">
-          <CircularProgress />
-          <p className="text-2xl text-white-primary">Đang tải dữ liệu...</p>
-        </div>
+        <Loading />
       ) : (
-        <Card className="w-full max-w-md">
+        <Card className="w-full xl:w-1/2 max-w-2xl">
           <CardHeader>
-            <CardTitle className="text-xl font-bold">
-              Đăng ký trở thành người bán
+            <CardTitle className="text-[1.5em] font-bold text-center">
+              {t("register_store")}
             </CardTitle>
           </CardHeader>
           <CardContent>
             <form onSubmit={registerStoreForm.handleSubmit(onSubmit)}>
-              <div className="grid w-full items-center gap-4">
+              <div className="grid w-full items-center gap-4 text-[1em]">
                 <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="username">Tên đăng nhập</Label>
+                  <span className="font-bold">{t("username")}</span>
                   <Input
                     id="username"
-                    placeholder="tên đăng nhập"
+                    placeholder={t("username")}
                     value={username}
                     disabled
                   />
                 </div>
                 <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Tên cửa hàng</Label>
+                  <span className="font-bold">{t("store_name")}</span>
                   <Input
                     id="name"
-                    placeholder="tên cửa hàng"
+                    placeholder={t("store_name")}
                     className="col-span-3"
                     {...registerStoreForm.register("name")}
                   />
                   {registerStoreForm.formState.errors.name && (
-                    <p className="text-sm text-error">
+                    <p className="text-sm text-red-primary">
                       {registerStoreForm.formState.errors.name.message}
                     </p>
                   )}
                 </div>
                 <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="bio">Bio</Label>
+                  <span className="font-bold">{t("bio")}</span>
                   <Textarea
                     id="bio"
-                    placeholder="bio"
+                    placeholder={t("bio")}
                     className="col-span-3"
                     {...registerStoreForm.register("bio")}
                   />
                   {registerStoreForm.formState.errors.bio && (
-                    <p className="text-sm text-error">
+                    <p className="text-sm text-red-primary">
                       {registerStoreForm.formState.errors.bio.message}
                     </p>
                   )}
                 </div>
               </div>
-              <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4 mt-4">
+              <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4 mt-4 font-bold">
                 <Button
                   type="button"
                   onClick={handleCancel}
-                  className="w-full sm:w-auto"
+                  className={`w-full h-10 text-[1em] shadow-sm shadow-black-secondary ${isSubmitting && 'cursor-not-allowed'} `}
                 >
-                  Hủy bỏ
+                  {t("cancel")}
                 </Button>
-                <Button type="submit" className="w-full sm:w-auto">
-                  Đăng ký
+                <Button type="submit" className={`w-full h-10 text-[1em] shadow-sm shadow-black-secondary relative ${isSubmitting && "cursor-wait"}`}>
+                  {isSubmitting ? <div className="global_loading_icon white"></div> : t("register")}
                 </Button>
               </div>
             </form>
