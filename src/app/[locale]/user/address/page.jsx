4938de88cf4b7079edcfe37b0e2f8cdd3456da45
chickenHuy <nguyenhuythanh256@gmail.com>
2025-04-18 20:29:56 +0700

    update UI address for /user

--- src/app/[locale]/user/address/page.jsx
+++ src/app/[locale]/user/address/page.jsx
@@ -88,122 +88,126 @@ export default function Component() {
   };
 
   return (
-    <div className="w-full h-fit lg:pl-[300px] flex justify-center items-center">
-      <Toaster className="z-50" />
-      <div className="min-w-[350px] w-[95%] px-5 py-10 shadow-xl rounded-xl flex flex-col items-center justify-between gap-5">
-        <div className="w-full flex flex-row justify-between items-center">
-          <h1 className="text-2xl font-bold">Địa chỉ của tôi</h1>
-          <Button
-            onClick={() => {
-              setSelectedId(null);
-              setShowAddressForm(true);
-            }}
-          >
-            <PlusCircle className="mr-2 h-4 w-4" />
-            Thêm địa chỉ mới
-          </Button>
-        </div>
-        <div className="w-full grid gap-4">
-          {addresses.length === 0 ? (
-            <Loading />
-          ) : (
-            addresses.map((address) => {
-              const {
-                id,
-                recipient_name,
-                phone,
-                first_line,
-                second_line,
-                is_default,
-                is_store_address,
-              } = address;
 
-              return (
-                <Card key={id}>
-                  <CardHeader className="flex flex-col lg:flex-row items-start justify-between pb-0">
-                    <CardTitle>
-                      {recipient_name} <span>({phone})</span>
-                    </CardTitle>
-                    <div className="flex flex-row justify-end w-full lg:w-fit gap-2">
-                      <Button
-                        onClick={() => {
-                          setSelectedId(id);
-                          setShowAddressForm(true);
-                        }}
-                      >
-                        Cập nhật
-                      </Button>
-                      {!is_default && (
+    addresses.length === 0 ? (
+      <div className="w-full h-fit lg:pl-[300px]">
+        <Loading />
+      </div>
+    ) : (
+      <div className="w-full h-fit lg:pl-[300px] flex justify-center items-center">
+        <Toaster className="z-50" />
+        <div className="min-w-[350px] w-[95%] px-5 py-10 shadow-xl rounded-xl flex flex-col items-center justify-between gap-5">
+          <div className="w-full flex flex-row justify-between items-center">
+            <h1 className="text-2xl font-bold">Địa chỉ của tôi</h1>
+            <Button
+              onClick={() => {
+                setSelectedId(null);
+                setShowAddressForm(true);
+              }}
+            >
+              <PlusCircle className="mr-2 h-4 w-4" />
+              Thêm địa chỉ mới
+            </Button>
+          </div>
+          <div className="w-full grid gap-4">
+            {
+              addresses?.map((address) => {
+                const {
+                  id,
+                  recipient_name,
+                  phone,
+                  first_line,
+                  second_line,
+                  is_default,
+                  is_store_address,
+                } = address;
+
+                return (
+                  <Card key={id}>
+                    <CardHeader className="flex flex-col lg:flex-row items-start justify-between pb-0">
+                      <CardTitle>
+                        {recipient_name} <span>({phone})</span>
+                      </CardTitle>
+                      <div className="flex flex-row justify-end w-full lg:w-fit gap-2">
                         <Button
-                          variant="outline"
-                          onClick={() => handleDeleteClick(address)}
+                          onClick={() => {
+                            setSelectedId(id);
+                            setShowAddressForm(true);
+                          }}
                         >
-                          Xóa
+                          Cập nhật
                         </Button>
-                      )}
-                    </div>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="space-y-1">
-                      <p className="text-sm">{first_line}</p>
-                      <p className="text-sm">{second_line}</p>
-                      <div className="pt-2 flex flex-wrap justify-start">
-                        <div>
-                          {is_default && (
-                            <span
-                              className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
-                            >
-                              Mặc định
-                            </span>
-                          )}
-                          {is_store_address && (
-                            <>
-                              <span
-                                className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
-                              >
-                                Địa chỉ lấy hàng
-                              </span>
-                              <span
-                                className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
-                              >
-                                Địa chỉ trả hàng
-                              </span>
-                            </>
-                          )}
-                        </div>
                         {!is_default && (
                           <Button
-                            onClick={() => handleSetDefault(id)}
+                            variant="outline"
+                            onClick={() => handleDeleteClick(address)}
                           >
-                            Thiết lập mặc định
+                            Xóa
                           </Button>
                         )}
                       </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              );
-            })
-          )}
+                    </CardHeader>
+                    <CardContent>
+                      <div className="space-y-1">
+                        <p className="text-sm">{first_line}</p>
+                        <p className="text-sm">{second_line}</p>
+                        <div className="pt-2 flex flex-wrap justify-start">
+                          <div>
+                            {is_default && (
+                              <span
+                                className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
+                              >
+                                Mặc định
+                              </span>
+                            )}
+                            {is_store_address && (
+                              <>
+                                <span
+                                  className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
+                                >
+                                  Địa chỉ lấy hàng
+                                </span>
+                                <span
+                                  className="text-sm text-red-primary border shadow-sm px-3 py-2 rounded-sm"
+                                >
+                                  Địa chỉ trả hàng
+                                </span>
+                              </>
+                            )}
+                          </div>
+                          {!is_default && (
+                            <Button
+                              onClick={() => handleSetDefault(id)}
+                            >
+                              Thiết lập mặc định
+                            </Button>
+                          )}
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                );
+              })
+            }
+          </div>
         </div>
-      </div>
 
-
-      <AddressForm
-        open={showAddressForm}
-        onOpenChange={setShowAddressForm}
-        id={selectedId}
-      />
-
-      {isDialogConfirmOpen && addressToDelete && (
-        <DialogConfirm
-          isOpen={isDialogConfirmOpen}
-          onClose={() => setIsDialogConfirmOpen(false)}
-          onConfirm={confirmDelete}
-          tableName="địa chỉ"
-          objectName={`${addressToDelete.first_line} + ${addressToDelete.second_line}`}
+        <AddressForm
+          open={showAddressForm}
+          onOpenChange={setShowAddressForm}
+          id={selectedId}
         />
-      )}
-    </div>
+
+        {isDialogConfirmOpen && addressToDelete && (
+          <DialogConfirm
+            isOpen={isDialogConfirmOpen}
+            onClose={() => setIsDialogConfirmOpen(false)}
+            onConfirm={confirmDelete}
+            tableName="địa chỉ"
+            objectName={`${addressToDelete.first_line} + ${addressToDelete.second_line}`}
+          />
+        )}
+      </div>
+    )
   );
 }
