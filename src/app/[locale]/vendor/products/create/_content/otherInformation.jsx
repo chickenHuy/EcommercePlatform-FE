a654c384197f91ef09792c0482017fe78d5e8bcb
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-12 21:11:56 +0700

    update UI for other information of create product

--- src/app/[locale]/vendor/products/create/_content/otherInformation.jsx
+++ src/app/[locale]/vendor/products/create/_content/otherInformation.jsx
@@ -5,17 +5,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useTranslations } from "next-intl";
 
 const OtherInformation = ({
   isDefaultDelivery = false,
   setIsDefaultDelivery = null,
 }) => {
+  const t = useTranslations("Vendor.create_product");
   return (
     <div className="text-[15px] w-full h-fit flex flex-col justify-center items-start px-5 gap-1">
-      <span className="font-[900] italic">Thông tin vận chuyển</span>
+      <span className="font-[900]">{t("shipping_information")}</span>
       <div className="w-full h-full lg:flex hidden flex-col justify-start items-start gap-3 p-5 shadow-md rounded-md border-[0.5px] border-white-secondary">
         <div className="my-2 w-full h-fit flex flex-row justify-start items-center gap-3">
-          <label className="text-sm">Sử dụng đơn vị vận chuyển mặc định</label>
+          <label className="text-sm">{t("use_default_delivery")}</label>
           <Select
             className="w-fit p-2 border border-gray-300 rounded mt-1"
             value={isDefaultDelivery ? "true" : "false"}
@@ -27,8 +29,8 @@ const OtherInformation = ({
               <SelectValue placeholder="Chọn trạng thái" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="true">Có</SelectItem>
-              <SelectItem value="false">Không</SelectItem>
+              <SelectItem value="true">{t("option_true")}</SelectItem>
+              <SelectItem value="false">{t("option_false")}</SelectItem>
             </SelectContent>
           </Select>
         </div>
