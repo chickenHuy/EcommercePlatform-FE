37d2bfe2ac04ef9fb06bc730a023343843bca9c2
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-18 00:28:54 +0700

    add new custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -115,6 +115,21 @@ module.exports = {
       "yellow-primary": "#FFD700",
       "red-primary": "#ef233b",
     },
+    animation: {
+      "fade-in": "fadeIn 1s ease-in-out forwards",
+      "fade-in-quick": "fadeIn 0.5s ease-in-out forwards",
+      "fade-in-up": "fadeInUp 1s ease-out forwards",
+    },
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: 0, transform: "scale(0.95)" },
+        "100%": { opacity: 1, transform: "scale(1)" },
+      },
+      fadeInUp: {
+        "0%": { opacity: "0", transform: "translateY(100px)" },
+        "100%": { opacity: "1", transform: "translateY(0)" },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
