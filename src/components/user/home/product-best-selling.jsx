777206ed6b2d7546eb9521e89176995db6297339
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-18 00:28:00 +0700

    make beautiful for best seling product component

--- src/components/user/home/product-best-selling.jsx
+++ src/components/user/home/product-best-selling.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef, useEffect, useState, useCallback } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChartNoAxesCombined, ChevronLeft, ChevronRight } from "lucide-react";
 import { getProductBestSelling } from "@/api/user/homeRequest";
 import ProductCard from "@/components/card/productCard";
 import { useTranslations } from "next-intl";
@@ -136,63 +136,66 @@ export default function ProductBestSelling() {
   };
 
   const SkeletonItem = () => (
-    <div className="skeleton-item w-[150px] h-[200px]">
-      <div className="skeleton-line w-full aspect-square" />
-      <div className="skeleton-line w-full h-full" />
+    <div className="skeleton-item flex flex-col lg:min-w-[250px] sm:min-w-[200px] min-w-[100px] aspect-[3/4]">
+      <div className="skeleton-line w-full h-3/4" />
+      <div className="skeleton-line w-full h-1/4" />
     </div>
   );
 
   return (
-    <div className="w-full pb-4">
+    <div className="w-full h-fit flex flex-row justify-start items-center bg-black-secondary lg:py-14 sm:py-10 py-8 rounded-xl shadow-sm">
       <Toaster />
-      <h3 className="text-[1.3em] text-center border-b pb-2">
-        {t("best_selling_products")}
-      </h3>
-
-      <div className="relative group">
-        <div
-          ref={scrollContainerRef}
-          className="flex flex-row overflow-x-auto gap-4 scroll-smooth no-scrollbar py-3"
-          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-        >
-          {isLoading
-            ? Array.from({ length: 20 }).map((_, index) => (
-                <SkeletonItem key={index} />
-              ))
-            : listProduct.map((product, index) => (
-                <div key={index} className="lg:min-w-[150px] sm:min-w-[130px] min-w-[100px]">
-                  <ProductCard
-                    key={product.id}
-                    name={product.name}
-                    price={product.salePrice}
-                    originalPrice={product.originalPrice}
-                    mainImageUrl={product.mainImageUrl}
-                    videoUrl={product.videoUrl}
-                    rating={product.rating}
-                    isFavorite={false}
-                    link={product.slug}
-                  />
-                </div>
-              ))}
-        </div>
-
-        <button
-          aria-label="Scroll Left"
-          onClick={() => scroll("left")}
-          className="absolute left-2 top-1/2 -translate-y-1/2 z-20 flex items-center justify-center w-10 h-10 rounded-full bg-white-primary/50 shadow-md hover:bg-white-primary hover:text-white"
-        >
-          <ChevronLeft className="h-5 w-5" />
-        </button>
-
+      <div className="lg:min-w-[200px] sm:min-w-[150px] min-w-[100px] text-white-primary flex flex-col justify-center items-center gap-3 h-fit px-3 relative">
+        <ChartNoAxesCombined className="lg:w-20 lg:h-20 w-10 h-10" />
+        <h3 className="lg:text-[1.3em] sm:text-[1.1em] text-[.9em] text-center">
+          {t("best_selling_products")}
+        </h3>
         {canScrollRight && (
           <button
-            aria-label="Scroll Right"
+            aria-label="Scroll Left"
             onClick={() => scroll("right")}
-            className="absolute right-2 top-1/2 -translate-y-1/2 z-20 flex items-center justify-center w-10 h-10 rounded-full bg-white-primary/50 shadow-md hover:bg-white-primary hover:text-white"
+            className="absolute left-1/2 -bottom-2 translate-y-full translate-x-1/3 z-20 flex items-center justify-center lg:p-3 p-[6px] rounded-full bg-white-primary/50 shadow-md hover:bg-white-primary hover:text-black-primary"
           >
             <ChevronRight className="h-5 w-5" />
           </button>
         )}
+
+        <button
+          aria-label="Scroll Right"
+          onClick={() => scroll("left")}
+          className="absolute right-1/2 -bottom-2 translate-y-full -translate-x-1/3 z-20 flex items-center justify-center lg:p-3 p-[6px] rounded-full bg-white-primary/50 shadow-md hover:bg-white-primary hover:text-black-primary"
+        >
+          <ChevronLeft className="h-5 w-5" />
+        </button>
+      </div>
+
+      <div
+        ref={scrollContainerRef}
+        className="flex flex-row overflow-x-auto lg:gap-5 gap-3 scroll-smooth no-scrollbar p-3"
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+      >
+        {isLoading
+          ? Array.from({ length: 20 }).map((_, index) => (
+              <SkeletonItem key={index} />
+            ))
+          : listProduct.map((product, index) => (
+              <div
+                key={index}
+                className="lg:min-w-[250px] sm:min-w-[200px] min-w-[100px]"
+              >
+                <ProductCard
+                  key={product.id}
+                  name={product.name}
+                  price={product.salePrice}
+                  originalPrice={product.originalPrice}
+                  mainImageUrl={product.mainImageUrl}
+                  videoUrl={product.videoUrl}
+                  rating={product.rating}
+                  isFavorite={false}
+                  link={product.slug}
+                />
+              </div>
+            ))}
       </div>
     </div>
   );
