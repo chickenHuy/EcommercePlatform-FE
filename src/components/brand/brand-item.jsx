de49c19b018160bc78cfbc74f2352956a3c1251b
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-18 20:10:01 +0700

    create brand component

---
+++ src/components/brand/brand-item.jsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import IconNotFound from "../../../public/images/categoryNotFound.png";
+import useInView from "@/hooks/use-visible";
+
+const BrandCard = ({ brand }) => {
+  const [ref, isInView] = useInView();
+  const containerRef = useRef(null);
+  const [size, setSize] = useState(0);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const observer = new ResizeObserver(([entry]) => {
+      const { width, height } = entry.contentRect;
+      const squareSize = Math.min(width, height);
+      setSize(squareSize);
+    });
+
+    observer.observe(containerRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div
+      ref={(el) => {
+        ref.current = el;
+        containerRef.current = el;
+      }}
+      style={{ height: brand.height }}
+      className={`w-full h-full flex flex-col items-center gap-2 relative group mb-3 sm:mb-5 ${isInView ? "animate-fade-in-up" : "opacity-0"}`}
+    >
+      <div
+        className="relative"
+        style={{
+          width: size,
+          height: size,
+        }}
+      >
+        <Image
+          src={brand.imageUrl || IconNotFound}
+          alt={brand.name}
+          fill
+          className="object-cover rounded-full shadow-md hover:scale-[1.03] transition duration-300"
+        />
+      </div>
+      {isInView && (
+        <p
+          style={{ top: size * 0.45, width: size * 0.8 }}
+          className="w-[80%] p-2 rounded-md absolute text-[.9em] text-white-primary text-center truncate backdrop-blur-sm bg-white-tertiary/50 group-hover:block hidden animate-fade-in-quick"
+        >
+          {brand.name.toUpperCase()}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default BrandCard;
---
+++ src/components/brand/index.jsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import IconNotFound from "../../../public/images/categoryNotFound.png";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import Masonry from "react-masonry-css";
+import BrandCard from "./brand-item";
+import { getAllBrand, getBrandPage } from "@/api/user/brandRequest";
+
+const ListBrandComponent = ({ isPage = false }) => {
+  const [listBrand, setListBrand] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isHovering, setIsHovering] = useState(false);
+  const scrollRef = useRef(null);
+  const t = useTranslations("Search");
+
+  useEffect(() => {
+    const fetchBrands = async () => {
+      setIsLoading(true);
+      try {
+        let response = null;
+        if (isPage) {
+          response = await getAllBrand();
+          const brands = response.result;
+
+          const brandsWithHeight = brands.map((element) => ({
+            ...element,
+            height: Math.random() * (window.innerWidth / 10) + 150 + window.innerWidth/30,
+          }));
+
+          setListBrand(brandsWithHeight);
+        } else {
+          response = await getBrandPage(20);
+          setListBrand(response.result.data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch brands:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchBrands();
+  }, []);
+
+  useEffect(() => {
+    if (!isPage && scrollRef.current && !isHovering) {
+      const interval = setInterval(() => {
+        const container = scrollRef.current;
+        if (
+          container.scrollLeft + container.offsetWidth >=
+          container.scrollWidth
+        ) {
+          container.scrollTo({ left: 0, behavior: "smooth" });
+        } else {
+          container.scrollBy({ left: 10, behavior: "smooth" });
+        }
+      }, 10);
+
+      return () => clearInterval(interval);
+    }
+  }, [isPage, isHovering]);
+
+  const SkeletonItem = () => {
+    if (isPage) {
+      return (
+        <div className="skeleton-circle w-full aspect-square relative sm:mb-5 mb-3"></div>
+      );
+    } else {
+      return (
+        <div className="skeleton-item lg:w-[200px] lg:h-[200px] sm:w-[150px] sm:h-[150px] w-[70px] h-[70px] relative">
+          <div className="skeleton-line w-[80%] sm:h-[40px] h-[20px] absolute bottom-5" />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="relative w-full h-fit">
+      <div className="w-full h-fit flex flex-row justify-between items-center px-10">
+        {!isPage ? (
+          <>
+            <h3 className="text-[1.2em] font-[900]">{t("list_brand")}</h3>
+            <Link className="underline" href="/brands">
+              {t("see_all")}
+            </Link>
+          </>
+        ) : (
+          <h3 className="sm:text-[2.2em] text-[1.5em] font-[900] w-full text-center animate-fade-in">
+            {t("list_brand").toUpperCase()}
+          </h3>
+        )}
+      </div>
+
+      {isPage ? (
+        <Masonry
+          breakpointCols={{ default: 5, 1024: 4, 767: 2 }}
+          className="main_grid_layout gap-3 sm:gap-5 mt-5"
+          columnClassName="main_grid_item"
+        >
+          {isLoading
+            ? Array.from({ length: 32 }).map((_, index) => (
+                <SkeletonItem key={index} />
+              ))
+            : listBrand.map((brand) => (
+                <BrandCard key={brand.id} brand={brand} />
+              ))}
+        </Masonry>
+      ) : (
+        <>
+          <div
+            ref={scrollRef}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+            className="flex gap-4 overflow-x-auto scroll-smooth no-scrollbar p-3 relative"
+          >
+            {isLoading
+              ? Array.from({ length: 20 }).map((_, index) => (
+                  <SkeletonItem key={index} />
+                ))
+              : listBrand.map((brand) => (
+                  <div
+                    key={brand.id}
+                    className="lg:w-[200px] lg:h-[200px] sm:w-[150px] sm:h-[150px] w-[70px] h-[70px] flex-shrink-0 flex flex-col items-center gap-2 relative group"
+                  >
+                    <div className="w-full h-full relative">
+                      <Image
+                        src={brand.imageUrl || IconNotFound}
+                        alt={brand.name}
+                        fill
+                        className="object-cover rounded-full shadow-md"
+                      />
+                    </div>
+                    <p className="w-[80%] sm:p-2 p-1 sm:rounded-md rounded-sm absolute top-[45%] lg:text-[.9em] sm:text-[.8em] text-[.7em] text-white-primary text-center truncate backdrop-blur-sm bg-white-tertiary/50 group-hover:block hidden animate-fade-in-quick">
+                      {brand.name.toUpperCase()}
+                    </p>
+                  </div>
+                ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListBrandComponent;
