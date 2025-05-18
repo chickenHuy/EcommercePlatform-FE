3f805d975382a0114cca928e77465a2ebef0b0b7
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-18 20:09:40 +0700

    create brands page

---
+++ src/app/[locale]/brands/page.jsx
@@ -0,0 +1,9 @@
+import ListBrandComponent from "@/components/brand";
+
+export default function BrandPage() {
+  return (
+    <div className="sm:py-24 py-20 xl:px-28 lg:px-20 sm:px-6 px-4">
+      <ListBrandComponent isPage={true} />
+    </div>
+  );
+}
