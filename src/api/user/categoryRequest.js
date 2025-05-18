9e94d744169b4f8d5f427075a4600625c0243880
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-18 15:47:59 +0700

    create API get category per page

--- src/api/user/categoryRequest.js
+++ src/api/user/categoryRequest.js
@@ -12,3 +12,15 @@ export const getAllCategory = () => {
   }
 };
 
+export const getCategoryPage = (sort = "asc", page = 1, size = 10, search = "") => {
+  try {
+    const response = get(
+      `/api/v1/categories?page=${page}&size=${size}&sort=${sort}&search=${search}`
+    );
+    return response;
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    throw error;
+  }
+}
+
