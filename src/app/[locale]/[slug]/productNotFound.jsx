1b6cd2ead16aa1ca7176fed1f8e2281f96741624
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-24 08:04:15 +0700

    update UI for product view not found

--- src/app/[locale]/[slug]/productNotFound.jsx
+++ src/app/[locale]/[slug]/productNotFound.jsx
@@ -2,24 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import Empty from "@/assets/images/reviewEmpty.png";
 
-const ProductNotFound = ({t}) => {
+const ProductNotFound = ({ t }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white-secondary/40">
+    <div className="flex flex-col items-center justify-center h-screen">
       <div className="text-center">
-        <h1 className="text-7xl font-extrabold text-red-primary">404</h1>
-        <p className="mt-4 text-2xl font-semibold text-black-primary">
-          {t("text_oops")}
-        </p>
-        <p className="mt-2 text-black-tertiary">
+        <p className="text-[1.5em] font-[900]">{t("text_oops")}</p>
+        <p className=" text-black-tertiary text-[1em]">
           {t("text_unavailable")}
         </p>
-        <Image
-          src={Empty} 
-          alt="Product Not Found"
-          className="w-1/2 mx-auto mt-6"
-        />
+        <Image src={Empty} alt="Product Not Found" className="w-1/2 mx-auto" />
         <Link href="/">
-          <button className="px-6 py-3 mt-6 font-medium text-white-primary bg-black-primary rounded-lg hover:bg-black-tertiary/40">
+          <button className="px-6 py-3 text-white-primary bg-black-primary rounded-lg hover:bg-black-primary/90">
             {t("text_back_home")}
           </button>
         </Link>
