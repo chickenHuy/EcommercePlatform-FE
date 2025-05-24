bfaf80361cf6bc30343ae970146ad7eca291664e
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-24 08:04:02 +0700

    update UI for product view skeleton

--- src/app/[locale]/[slug]/product-skeleton.jsx
+++ src/app/[locale]/[slug]/product-skeleton.jsx
@@ -2,87 +2,88 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ProductDetailSkeleton() {
   return (
-    <div className="flex-col bg-opacity-60 bg-blue-primary">
-      <div className="mx-auto px-4 h-1 bg-blue-primary w-3/4"></div>
-      <div className="mx-auto px-4 bg-white-primary mt-20 w-3/4">
-        <div className="grid gap-8 md:grid-cols-2 mt-2">
-          {/* Product Media Viewer Skeleton */}
-          <Skeleton className="h-64 w-full rounded-lg" />
+    <div className="w-full h-fit xl:px-28 lg:px-20 sm:px-6 px-4 py-20 space-y-3">
+      <div className="w-full h-fit flex flex-col shadow-md rounded-md p-3">
+        <div className="w-full h-fit">
+          <div className="grid gap-7 sm:grid-cols-2 grid-cols-1">
+            <div className="skeleton-item w-full aspect-[4/3] rounded-lg"></div>
 
-          <div className="space-y-6 mt-2">
-            {/* Product Title */}
-            <Skeleton className="h-8 w-3/4" />
+            <div className="space-y-5">
+              <div className="skeleton-item h-8 w-full"></div>
 
-            {/* Product Ratings */}
-            <div className="mt-2 flex items-center space-x-2">
-              <Skeleton className="h-5 w-24" />
-              <Skeleton className="h-5 w-16" />
-            </div>
-
-            {/* Separator */}
-            <Skeleton className="h-1 w-full" />
-
-            {/* Product Description */}
-            <Skeleton className="h-6 w-full" />
-            <Skeleton className="h-6 w-5/6" />
-            <Skeleton className="h-6 w-4/6" />
-
-            {/* Pricing */}
-            <Skeleton className="h-10 w-1/2" />
-
-            {/* Attributes */}
-            <div>
-              <Skeleton className="h-6 w-1/4" />
-              <div className="mt-2 flex space-x-2">
-                {[1, 2, 3].map((_, idx) => (
-                  <Skeleton key={idx} className="h-8 w-20 rounded" />
-                ))}
+              <div className="flex items-center flex-wrap space-x-2">
+                <div className="skeleton-item h-8 w-1/4"></div>
+                <div className="skeleton-item h-8 w-1/4"></div>
               </div>
-            </div>
 
-            {/* Quantity */}
-            <div>
-              <Skeleton className="h-6 w-1/4" />
-              <div className="mt-2 flex items-center space-x-2">
-                <Skeleton className="h-8 w-8 rounded" />
-                <Skeleton className="h-8 w-12" />
-                <Skeleton className="h-8 w-8 rounded" />
-                <Skeleton className="h-5 w-1/4" />
+              <Skeleton className="h-[2px] w-full" />
+
+              <div className="space-y-1">
+                <div className="skeleton-item h-8 w-full"></div>
+                <div className="skeleton-item h-8 w-full"></div>
+                <div className="skeleton-item h-8 w-full"></div>
               </div>
-            </div>
 
-            {/* Buttons */}
-            <div className="flex space-x-4">
-              <Skeleton className="h-10 w-1/2 rounded" />
-              <Skeleton className="h-10 w-10 rounded" />
-            </div>
-          </div>
-        </div>
-      </div>
+              <div className="skeleton-item h-10 w-1/2"></div>
 
-      {/* Store Info */}
-      <div className="mx-auto px-4 bg-blue-primary rounded-lg">
-        <div className="bg-blue-primary border-none">
-          <div className="p-4 w-3/4 mx-auto text-center">
-            <Skeleton className="h-6 w-1/2" />
-            <div className="mt-4 flex mx-auto w-fit items-center space-x-4">
-              <Skeleton className="h-16 w-16 rounded-full" />
               <div>
-                <Skeleton className="h-5 w-32" />
-                <Skeleton className="h-4 w-24" />
+                <div className="skeleton-item h-8 w-1/2"></div>
+                <div className="mt-2 flex gap-2 flex-wrap">
+                  {[1, 2, 3].map((_, idx) => (
+                    <div key={idx} className="skeleton-item h-8 w-[16%]"></div>
+                  ))}
+                </div>
               </div>
-              <Skeleton className="h-10 w-24 rounded" />
+
+              <div>
+                <div className="skeleton-item h-8 w-1/2"></div>
+                <div className="mt-2 flex gap-2 flex-wrap">
+                  {[1, 2, 3].map((_, idx) => (
+                    <div key={idx} className="skeleton-item h-8 w-[16%]"></div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex space-x-2">
+                <div className="skeleton-item h-10 w-1/2"></div>
+                <div className="skeleton-item h-10 w-1/2"></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      {/* Product Details */}
-      <div className="mx-auto px-4 rounded-lg w-3/4">
-        <Skeleton className="h-6 w-1/3" />
-        <Skeleton className="h-5 w-full" />
-        <Skeleton className="h-5 w-5/6" />
-        <Skeleton className="h-5 w-4/6" />
+      <div className="w-full h-fit flex flex-col justify-center items-center shadow-md rounded-md gap-3 p-3">
+        <div className="skeleton-item h-8 w-[300px]"></div>
+        <div className="skeleton-circle h-24 w-24"></div>
+        <div className="skeleton-item h-8 w-[200px]"></div>
+        <div className="w-fit flex flex-row gap-3">
+          <div className="skeleton-item h-8 w-[150px]"></div>
+          <div className="skeleton-item h-8 w-[150px]"></div>
+        </div>
+      </div>
+
+      <div className="w-full h-fit flex flex-col shadow-md rounded-md p-3 space-y-2">
+        <div className="space-x-2 flex">
+          <div className="skeleton-item h-8 w-1/2"></div>
+          <div className="skeleton-item h-8 w-1/2"></div>
+        </div>
+        <div className="space-x-2 flex">
+          <div className="skeleton-item h-8 w-1/2"></div>
+          <div className="skeleton-item h-8 w-1/2"></div>
+        </div>
+        <div className="space-x-2 flex">
+          <div className="skeleton-item h-8 w-1/2"></div>
+          <div className="skeleton-item h-8 w-1/2"></div>
+        </div>
+        <div className="space-x-2 flex">
+          <div className="skeleton-item h-8 w-1/2"></div>
+          <div className="skeleton-item h-8 w-1/2"></div>
+        </div>
+        <div className="space-x-2 flex">
+          <div className="skeleton-item h-8 w-1/2"></div>
+          <div className="skeleton-item h-8 w-1/2"></div>
+        </div>
       </div>
     </div>
   );
