7763d9114631aeca38dc6326733bda55c664d082
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-28 23:10:40 +0700

    fix redirectUrl

--- src/configs/oauthConfig.js
+++ src/configs/oauthConfig.js
@@ -1,11 +1,11 @@
 export const OAuthConfig = {
   clientId: "114329022200-hkajl8098uino3u4hjl1e30gmph9m80l.apps.googleusercontent.com",
-  redirectUri: `${process.env.NEXT_PUBLIC_BASE_URL}/authenticate`,
+  redirectUri: `https://nguyenthanhhuy.id.vn/authenticate`,
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
 
 export const FacebookOAuthConfig = {
   clientId: "1699360190637263",
-  redirectUri: `${process.env.NEXT_PUBLIC_BASE_URL}/facebook-authenticate`,
+  redirectUri: `https://nguyenthanhhuy.id.vn/facebook-authenticate`,
   authUri: "https://facebook.com/v21.0/dialog/oauth",
 };
