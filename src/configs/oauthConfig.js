1a1ebdc90a2a39eda50e450b5cd6fd325a0043ba
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-28 23:25:28 +0700

    change redirectUrl

--- src/configs/oauthConfig.js
+++ src/configs/oauthConfig.js
@@ -1,11 +1,11 @@
 export const OAuthConfig = {
   clientId: "114329022200-hkajl8098uino3u4hjl1e30gmph9m80l.apps.googleusercontent.com",
-  redirectUri: `https://nguyenthanhhuy.id.vn/authenticate`,
+  redirectUri: `https://hkk.nguyenthanhhuy.id.vn/authenticate`,
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
 
 export const FacebookOAuthConfig = {
   clientId: "1699360190637263",
-  redirectUri: `https://nguyenthanhhuy.id.vn/facebook-authenticate`,
+  redirectUri: `https://hkk.nguyenthanhhuy.id.vn/facebook-authenticate`,
   authUri: "https://facebook.com/v21.0/dialog/oauth",
 };
