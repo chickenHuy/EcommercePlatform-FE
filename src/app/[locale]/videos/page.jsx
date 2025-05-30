e65fc1c6d5fbaddb981276db6d081010b1b8e16a
chickenHuy <nguyenhuythanh256@gmail.com>
2025-05-30 19:05:01 +0000

    update UI for tiktok page

--- src/app/[locale]/videos/page.jsx
+++ src/app/[locale]/videos/page.jsx
@@ -3,5 +3,9 @@
 import TikTokProductViewer from "@/components/videos/tik-tok-product-viewer";
 
 export default function Page() {
-  return <TikTokProductViewer initialSize={4} searchPlaceholder=""  />
+  return (
+    <div className="w-full h-screen xl:px-28 lg:px-20 sm:px-6 px-4 pt-20 pb-1">
+      <TikTokProductViewer initialSize={10} />
+    </div>
+  )
 }
