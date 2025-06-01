bcc03946e6ba4a995195b831f7848fc8a4ba13f3
chickenHuy <nguyenhuythanh256@gmail.com>
2025-06-01 12:49:10 +0000

    update component addredd of checkout page

--- src/app/[locale]/checkout/address.jsx
+++ src/app/[locale]/checkout/address.jsx
@@ -8,7 +8,7 @@ import ListAddress from "./listAddressPopup";
 
 export default function Address(props) {
 
-    const [addresses, setAddresses] = useState([]);
+    const [addresses, setAddresses] = useState(null);
     const { defaultAddress, setDefaultAddress, t } = props;
     const page = 1;
     const itemsPerPage = 10;
@@ -34,41 +34,41 @@ export default function Address(props) {
     }
 
     return (
-        <Card className="m-4 border-0 rounded-none rounded-b-xl border-t-[4px] border-red-primary p-3">
-            <CardHeader className="py-0 px-[7px] ">
-                <div className="flex flex-row justify-start items-center">
-                    <MapPin className="mr-2 h-4 w-4 text-muted-foreground" />
-                    <CardTitle className="text-base">{t("text_shipping_address")}</CardTitle>
+        <Card className="w-full rounded-md p-3 border-t-[5px] border-t-black-primary animate-fade-in">
+            <CardHeader className="p-0">
+                <div className="flex flex-row gap-3 justify-start items-center">
+                    <MapPin className="h-5 w-5 text-muted-foreground" />
+                    <CardTitle className="text-[1em]">{t("text_shipping_address")}</CardTitle>
                 </div>
             </CardHeader>
             <CardContent>
-                <div className="flex justify-between">
+                <div className="flex flex-row flex-wrap gap-1 justify-between item-center">
                     {
                         defaultAddress ? (
-                            <div className="space-y-1 flex justify-between items-start w-full">
-                                <div>
-                                    <div className="font-medium">{defaultAddress?.recipient_name} ({defaultAddress?.phone})</div>
-                                    <div className="text-sm text-muted-foreground truncate">{defaultAddress?.first_line + defaultAddress?.second_line}</div>
+                            <div className="flex flex-row flex-wrap flex-grow max-w-full gap-1 justify-between items-center">
+                                <div className="w-full">
+                                    <div className="text-[1em]">{defaultAddress?.recipient_name} ({defaultAddress?.phone})</div>
+                                    <div className="text-[.9em] text-muted-foreground truncate w-full">{defaultAddress?.first_line + defaultAddress?.second_line}</div>
                                 </div>
                                 {
                                     defaultAddress?.is_default ?
-                                        (<div className="text-red-primary/90 mr-6 text-sm">
+                                        (<div className="text-red-primary text-[.9em]">
                                             {t("text_default")}
                                         </div>) : null
                                 }
                             </div>
                         ) : (
-                            <div className="text-sm">{t("text_not_address")}</div>
+                            <div className="text-[.9em]">{t("text_not_address")}</div>
                         )
                     }
                     <div className="">
                         <Button onClick={() => handleChangeAddress()}>
                             {t("button_change")}
                         </Button>
+                        <ListAddress open={open} onOpenChange={setOpen} setHasNew={setHasNew} hasNew={hasNew} setDefaultAddress={setDefaultAddress} defaultId={defaultAddress?.id} addresses={addresses} t={t} />
                     </div>
                 </div>
             </CardContent>
-            <ListAddress open={open} onOpenChange={setOpen} setHasNew={setHasNew} hasNew={hasNew} setDefaultAddress={setDefaultAddress} defaultId={defaultAddress?.id} addresses={addresses} t={t} />
         </Card>
     )
 }
